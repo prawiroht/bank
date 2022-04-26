6401c2c997c97cf47a124940b4198cc301076fea
DESKTOP-PCDMJK8\E7450 <galihsahar21@gmail.com>
2022-04-26 10:55:59 +0700

    Make relation between entity

--- backend/src/main/java/com/bank/backend/entity/Period.java
+++ backend/src/main/java/com/bank/backend/entity/Period.java
@@ -1,5 +1,31 @@
 package com.bank.backend.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "PERIODS")
 public class Period {
+    @Id
+    @GeneratedValue(generator = "PERIOD_GEN", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "PERIOD_GEN", sequenceName = "PERIOD_SEQ", allocationSize = 1)
+    private Long periodId;
+    @Column(name = "PERIOD")
+    @Temporal(TemporalType.DATE)
+    private Date period;
 
 }
