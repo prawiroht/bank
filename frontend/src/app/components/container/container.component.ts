058b14ee4f3c1be7796c1fbe4b162eb4b697a742
DESKTOP-DTVR69N\dell <yusroayyun26@gmail.com>
2022-04-26 14:42:26 +0700

    kerangka tabel

--- frontend/src/app/components/container/container.component.ts
+++ frontend/src/app/components/container/container.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
+  containers: [] = [];
+  first = 0;
+  rows = 10;
+  searchVal = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  next(){
+    this.first = this.first + this.rows;
+  }
+
+  prev(){
+    this.first = this.first - this.rows;
+  }
+
+  reset(){
+    this.first = 0;
+  }
+
+  isLastPage(): boolean{
+    if (this.containers != null){
+      if(this.containers.length < this.rows){
+        return true;
+      }
+      else{
+        return (this.containers.length - this.first <= this.rows);
+      }
+    }
+    return true;
+  }
+
+  isFirstPage():boolean{
+    return this.containers?this.first === 0 : true;
+  }
+
+  loadData(){
+
   }
 
 }
--- frontend/src/app/components/expenditure/expenditure.component.ts
+++ frontend/src/app/components/expenditure/expenditure.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenditureComponent implements OnInit {
 
+  expenditures: [] = [];
+  first = 0;
+  rows = 10;
+  searchVal = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  next(){
+    this.first = this.first + this.rows;
+  }
+
+  prev(){
+    this.first = this.first - this.rows;
+  }
+
+  reset(){
+    this.first = 0;
+  }
+
+  isLastPage(): boolean{
+    if (this.expenditures != null){
+      if(this.expenditures.length < this.rows){
+        return true;
+      }
+      else{
+        return (this.expenditures.length - this.first <= this.rows);
+      }
+    }
+    return true;
+  }
+
+  isFirstPage():boolean{
+    return this.expenditures?this.first === 0 : true;
+  }
+
+  loadData(){
+
   }
 
 }
