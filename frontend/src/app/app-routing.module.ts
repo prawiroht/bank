7380fce16413be99738380ddb0e6879a831521c6
RayhanBryan <rayhanbryan@gmail.com>
2022-04-26 10:33:43 +0700

    bikin component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'user-management', component: UserManagementComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { GroupManagementComponent } from './components/group-management/group-management.component';
+import { BankComponent } from './components/bank/bank.component';
+import { FundComponent } from './components/fund/fund.component';
+import { ReceivingComponent } from './components/receiving/receiving.component';
+import { PurchaseComponent } from './components/purchase/purchase.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserManagementComponent,
+    GroupManagementComponent,
+    BankComponent,
+    FundComponent,
+    ReceivingComponent,
+    PurchaseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AccordionModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
