45c5767ebe084747d5bb2d33da6bd1dd0cfb2b16
DESKTOP-00LSPAA\E7450 <lukypancap@gmail.com>
2022-04-26 10:11:43 +0700

    Added Fund

---
+++ backend/src/main/java/com/bank/backend/entity/Fund.java
@@ -0,0 +1,28 @@
+package com.bank.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "FUNDS")
+public class Fund {
+    
+    @Id
+    @Column(name = "FUND_ID")
+    @GeneratedValue(generator = "FUND_GEN", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "FUND_GEN", sequenceName = "FUNDS_SEQ", allocationSize = 1)
+    private Long fundId;
+    
+    @Column(name = "FUND_NAME")
+    private String fundName;
+}
