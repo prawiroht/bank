0a9d80142b8a2120c7d1a71b9dd79e8bc79111ec
Toto Budi Trapsilo <toto2441999@gmail.com>
2022-04-26 11:33:07 +0700

    Add home route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApprovalSaldoComponent } from './components/approval-saldo/approval-saldo.component';
+import { ApprovalTransactionComponent } from './components/approval-transaction/approval-transaction.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserManagementComponent } from './components/user-management/user-management.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'user-management', component: UserManagementComponent }
+  { path: 'user-management', component: UserManagementComponent },
+  { path: 'approval-saldo', component: ApprovalSaldoComponent },
+  { path: 'user-management', component: UserManagementComponent },
+  { path: 'approval-transaction', component: ApprovalTransactionComponent },
+  { path: 'home', component: DashboardComponent},
 ];
 
 @NgModule({
