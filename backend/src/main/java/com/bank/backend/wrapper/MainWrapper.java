95c91e0f144ae2c2f0ecb8bb7be46e49a93d8b4f
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-27 08:08:34 +0700

    entity, repo, wrapper Main

---
+++ backend/src/main/java/com/bank/backend/wrapper/MainWrapper.java
@@ -0,0 +1,27 @@
+package com.bank.backend.wrapper;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MainWrapper {
+    private Long mainId;
+    private Long universityId;
+    private String universityName;
+    private String bankId;
+    private String bankName;
+    private String accountNumber;
+    private String mutationId;
+    private Date transactionDate;
+    private Long value;
+    private Long purchaseId;
+    private String purchaseName;
+    private Long accountTypeId;
+    private String accountTypeName;
+    private Long fundId;
+    private String fundName;
+    private String description;
+}
