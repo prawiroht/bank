94c3e6189066b939f0eb0e846b01c55775109060
Toto Budi Trapsilo <toto2441999@gmail.com>
2022-04-27 08:38:56 +0700

    Add balance and transaction approval

--- frontend/src/app/components/approval-transaction/approval-transaction.component.ts
+++ frontend/src/app/components/approval-transaction/approval-transaction.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApprovalTransactionComponent implements OnInit {
 
+  page: number = 0;
+  data: any;
+  first = 0;
+  rows = 10;
+  searchVal = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData(this.page);
+  }
+
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.data ? this.first === (this.data.length - this.rows) : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.data ? this.first === 0 : true;
+  }
+
+  loadData(page: number) {
+    // this.dataervice.getUser(page).subscribe(
+    //   res => {
+    //     console.log(res.data)
+    //     this.data = res.data;
+    //   }
+    // )
   }
 
 }
