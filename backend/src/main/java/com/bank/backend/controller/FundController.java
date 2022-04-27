f6c64969276989bf7b70201da7c3e16ca6038b80
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-27 13:07:52 +0700

    add cross origin

--- backend/src/main/java/com/bank/backend/controller/FundController.java
+++ backend/src/main/java/com/bank/backend/controller/FundController.java
@@ -7,6 +7,7 @@ import com.bank.backend.util.DataResponseList;
 import com.bank.backend.util.DataResponsePagination;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 @RequestMapping (path = "/fund")
 public class FundController {
