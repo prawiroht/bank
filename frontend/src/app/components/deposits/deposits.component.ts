299eec49ba1e8239664ecf5d3730a16b2a8b53e9
DESKTOP-PCSOFBC\dell <cindyhilma1998@gmail.com>
2022-04-27 14:32:34 +0700

    tabel deposito

--- frontend/src/app/components/deposits/deposits.component.ts
+++ frontend/src/app/components/deposits/deposits.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DepositsService } from 'src/app/services/deposits.service';
+
 
 @Component({
   selector: 'app-deposits',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deposits.component.css']
 })
 export class DepositsComponent implements OnInit {
+  page : number = 0;
+  deposito: any;
+  first = 0;
+  rows = 10;
+  searchVal = '';
 
-  constructor() { }
+  constructor(
+    private depositsService :DepositsService
+  ) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  next(){
+    this.first = this.first + this.rows;
+  }
+
+  prev(){
+    this.first = this.first - this.rows;
+  }
+
+  reset(){
+    this.first = 0;
+  }
+
+  isLastPage(): boolean{
+    if (this.deposito != null){
+      if(this.deposito.length < this.rows){
+        return true;
+      }
+      else{
+        return (this.deposito.length - this.first <= this.rows);
+      }
+    }
+    return true;
+  }
+
+  isFirstPage():boolean{
+    return this.deposito?this.first === 0 : true;
+  }
+
+  loadData(){
+    this.depositsService.getDeposits().subscribe (
+      (      res: { data: any; })  => {
+        console.log(res.data)
+        this.deposito = res.data;
+      }
+    )
   }
 
 }
--- frontend/src/app/services/deposits.service.ts
+++ frontend/src/app/services/deposits.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+const url: string = environment.url;
+const httpOptions = {
+  headers: new HttpHeaders({
+    Accept: 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepositsService {
+  private depositsUrl = url + 'deposit/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDeposits(): Observable<any> {
+    return this.http.get<any>(this.depositsUrl + `findAll`, httpOptions);
+  }
 }
