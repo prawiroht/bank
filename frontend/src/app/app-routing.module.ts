8475bbc6c434aa72874bee7a84af32ef441124bf
DESKTOP-PCSOFBC\dell <cindyhilma1998@gmail.com>
2022-04-27 14:25:41 +0700

    add services deposits dan investment

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,8 +7,10 @@ import { ContainerComponent } from './components/container/container.component';
 import { DashboardContainerComponent } from './components/dashboard-container/dashboard-container.component';
 import { DashboardExpenditureComponent } from './components/dashboard-expenditure/dashboard-expenditure.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DepositsComponent } from './components/deposits/deposits.component';
 import { ExpenditureComponent } from './components/expenditure/expenditure.component';
 import { GiroComponent } from './components/giro/giro.component';
+import { InvestmentComponent } from './components/investment/investment.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
 import { UserManagementComponent } from './components/user-management/user-management.component';
@@ -22,6 +24,8 @@ const routes: Routes = [
   { path: 'expenditure', component: ExpenditureComponent, canActivate: [AuthGuardService] },
   { path: 'container', component: ContainerComponent, canActivate: [AuthGuardService] },
   { path: 'giro', component: GiroComponent, canActivate: [AuthGuardService] },
+  { path: 'deposits', component: DepositsComponent, canActivate: [AuthGuardService] },
+  { path: 'investment', component: InvestmentComponent, canActivate: [AuthGuardService] },
   { path: 'deposit-approval', component: ApprovalSaldoDepositoComponent, canActivate: [AuthGuardService] },
   { path: 'giro-approval', component: ApprovalSaldoGiroComponent, },
   { path: 'investation-approval', component: ApprovalSaldoInvestasiComponent, canActivate: [AuthGuardService] },
