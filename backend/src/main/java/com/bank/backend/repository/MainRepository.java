74bc0f7612af94600a7309209d6fafe29c1e3838
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-28 13:22:12 +0700

    main repo, controller edited

--- backend/src/main/java/com/bank/backend/repository/MainRepository.java
+++ backend/src/main/java/com/bank/backend/repository/MainRepository.java
@@ -16,15 +16,13 @@ public interface MainRepository extends JpaRepository<Main, Long>{
     @Query(value = "SELECT m.MAIN_ID, m.UNIVERSITY_ID, m.BANK_ID, m.TRANSACTION_DATE,"+ 
                         "m.PURCHASE_ID, m.ACCOUNT_TYPE_ID, m.ACCOUNT_NUMBER, m.MUTATION_ID,"+ 
                         "m.FUND_ID, m.VALUE, m.USER_ID, m.STATUS_ID,m.DESCRIPTION FROM MAINS m " +
-                        "LEFT JOIN UNIVERSITIES u ON u.UNIVERSITY_ID = m.UNIVERSITY_ID " +
                         "LEFT JOIN BANKS b ON b.BANK_ID = m.BANK_ID " +
                         "LEFT JOIN PURCHASES p ON p.PURCHASE_ID = m.PURCHASE_ID " +
                         "LEFT JOIN ACCOUNT_TYPES a ON a.ACCOUNT_TYPE_ID = m.ACCOUNT_TYPE_ID "+
                         "LEFT JOIN FUNDS f ON f.FUND_ID = m.FUND_ID "+
                         "LEFT JOIN STATUSES s ON s.STATUS_ID = m.STATUS_ID "+
                         "LEFT JOIN USERS ui ON ui.USER_ID = m.USER_ID "+
-                        "WHERE LOWER(u.UNIVERSITY_NAME) " +
-                        "LIKE LOWER(CONCAT(CONCAT('%',:pUniversityName),'%')) OR LOWER(b.CODE) " +
+                        "WHERE LOWER(b.CODE) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pCode), '%')) OR LOWER(b.BANK_NAME) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pBankName), '%')) OR LOWER(p.PURCHASE_NAME) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pPurchaseName), '%')) OR LOWER(a.ACCOUNT_TYPE_NAME) " +
@@ -33,15 +31,13 @@ public interface MainRepository extends JpaRepository<Main, Long>{
                         "LIKE LOWER(CONCAT(CONCAT('%',:pMutationId), '%')) OR LOWER(f.FUND_NAME) "+
                         "LIKE LOWER(CONCAT(CONCAT('%',:pFundName),'%'))",
                         countQuery = "SELECT COUNT(*) FROM MAINS m " +
-                        "LEFT JOIN UNIVERSITIES u ON u.UNIVERSITY_ID = m.UNIVERSITY_ID " +
                         "LEFT JOIN BANKS b ON b.BANK_ID = m.BANK_ID " +
                         "LEFT JOIN PURCHASES p ON p.PURCHASE_ID = m.PURCHASE_ID " +
                         "LEFT JOIN ACCOUNT_TYPES a ON a.ACCOUNT_TYPE_ID = m.ACCOUNT_TYPE_ID "+
                         "LEFT JOIN FUNDS f ON f.FUND_ID = m.FUND_ID "+
                         "LEFT JOIN STATUSES s ON s.STATUS_ID = m.STATUS_ID "+
                         "LEFT JOIN USERS ui ON ui.USER_ID = m.USER_ID "+
-                        "WHERE LOWER(u.UNIVERSITY_NAME) " +
-                        "LIKE LOWER(CONCAT(CONCAT('%',:pUniversityName),'%')) OR LOWER(b.CODE) " +
+                        "WHERE LOWER(b.CODE) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pCode), '%')) OR LOWER(b.BANK_NAME) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pBankName), '%')) OR LOWER(p.PURCHASE_NAME) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pPurchaseName), '%')) OR LOWER(a.ACCOUNT_TYPE_NAME) " +
@@ -49,8 +45,7 @@ public interface MainRepository extends JpaRepository<Main, Long>{
                         "LIKE LOWER(CONCAT(CONCAT('%',:pAccountNumber), '%')) OR LOWER(m.MUTATION_ID) " +
                         "LIKE LOWER(CONCAT(CONCAT('%',:pMutationId), '%')) OR LOWER(f.FUND_NAME) "+
                         "LIKE LOWER(CONCAT(CONCAT('%',:pFundName),'%'))", nativeQuery = true)
-        Page<Main> getByAllCategorie(@Param("pUniversityName") String universityName,
-                        @Param("pCode") String code,
+        Page<Main> getByAllCategorie(@Param("pCode") String code,
                         @Param("pBankName") String bankName,
                         @Param("pPurchaseName") String purchaseName,
                         @Param("pAccountTypeName") String accountTypeName,
@@ -60,7 +55,7 @@ public interface MainRepository extends JpaRepository<Main, Long>{
                         Pageable page);
 
         default Page<Main> getByAllCategories(String all, Pageable page) {
-                return getByAllCategorie(all, all, all, all, all, all, all, all, page);
+                return getByAllCategorie(all, all, all, all, all, all, all, page);
         }
 
         Page<Main> findByStatus(Status status,Pageable page);
