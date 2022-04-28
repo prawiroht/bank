4bbb2ef97984852065c5408cd43f3e1a276b4e2b
prawiroht <prawiroht@gmail.com>
2022-04-28 08:55:40 +0700

    user : "goodbye optional"

--- backend/src/main/java/com/bank/backend/repository/UserRepository.java
+++ backend/src/main/java/com/bank/backend/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package com.bank.backend.repository;
 
-import java.util.Optional;
-
 import com.bank.backend.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long>{
-    public Optional<User> findByUsername(String username);
+    public User getByUsername(String username);
 
-    public Optional<User> findByEmail(String email);
+    public User getByEmail(String email);
 }
